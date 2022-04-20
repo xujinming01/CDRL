0924a4c92934a99d62acd61b7ef472e72f7bde5f
xujinming01 <jinming01@qq.com>
2022-04-20 21:12:05 +0800

    Add CLI to stone.py

--- stone.py
+++ stone.py
@@ -1,10 +1,11 @@
-"""Testing for directly outputting discrete actions."""
+"""Directly outputting discrete actions."""
 
 import os
-import timeit
+import time
 
+import fire
 import gym
-from stable_baselines3 import SAC
+from stable_baselines3 import DDPG, SAC
 from stable_baselines3.common.callbacks import CallbackList
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.callbacks import StopTrainingOnMaxEpisodes
@@ -17,27 +18,34 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 # import gym_goal
 import gym_platform
 
+ALGORITHMS = {"ddpg": DDPG,
+              "sac": SAC}
 
-def main():
-    log_directory = "log/"
-    os.makedirs(log_directory, exist_ok=True)
 
-    n_runs = 5
+def run(algo: str = "ddpg",
+        n_runs: int = 5,
+        max_timesteps: int = 2_000_000,
+        max_episodes: int = 80_000,
+        n_eval_episodes: int = 100,
+        eval_freq: int = 20_000,
+        ):
+    logs_dir = f"log/{algo}_stone/platform/"  # Make sure of using right path
+    algo = ALGORITHMS[algo]
+    os.makedirs(logs_dir, exist_ok=True)
+
     for i in range(n_runs):
-        log_dir = log_directory + f"{i + 2}"
-        learn(log_dir)
+        log_dir = logs_dir + f"{i}"
+        learn_single_run(log_dir, algo, max_timesteps, max_episodes,
+                         n_eval_episodes, eval_freq)
 
 
-def learn(log_dir):
+def learn_single_run(log_dir, algo, max_timesteps, max_episodes,
+                     n_eval_episodes, eval_freq):
     # Save log to *.monitor.csv
     env = Monitor(gym.make('Platform-v0'), log_dir)
-    env = DummyVecEnv([lambda: env])  # SAC only support VecEnv
+    env = DummyVecEnv([lambda: env])
 
-    # n_actions = env.action_space.shape[-1]
-    # action_noise = NormalActionNoise(mean=np.zeros(n_actions),
-    #                                  sigma=float(0.02) * np.ones(n_actions))
-
-    model = SAC(
+    model = algo(
         policy='MultiInputPolicy',
         env=env,
         # gamma=0.99,
@@ -48,31 +56,28 @@ def learn(log_dir):
         # action_noise=action_noise,
         # tensorboard_log="tensorboard_outputs/"
     )
+
     # set up logger
     output_format = [make_output_format("csv", log_dir)]
     logger = Logger(folder=log_dir, output_formats=output_format)
     model.set_logger(logger)
 
-    callback_max_episodes = StopTrainingOnMaxEpisodes(max_episodes=80000,
-                                                      verbose=1)
-    # Separate evaluation env
-    env_eval = gym.make('Platform-v0')
+    callback_max_episodes = StopTrainingOnMaxEpisodes(max_episodes, verbose=1)
+    env_eval = gym.make('Platform-v0')  # Separate evaluation env
     callback_eval = EvalCallback(env_eval,
                                  best_model_save_path=log_dir,
                                  log_path=log_dir,
-                                 n_eval_episodes=100,
-                                 eval_freq=20000)
+                                 n_eval_episodes=n_eval_episodes,
+                                 eval_freq=eval_freq)
     callback = CallbackList([callback_max_episodes, callback_eval])
 
-    start = timeit.default_timer()  # Time the training
+    start = time.time()  # Time the training
     model.learn(
-        total_timesteps=1_000_000,
+        total_timesteps=max_timesteps,
         callback=callback
     )
-    stop = timeit.default_timer()
-
-    print(f"\nTraining time in seconds: {int(stop - start)}")
+    print(f"\tTraining time in seconds: {int(time.time() - start)}")
 
 
 if __name__ == '__main__':
-    main()
+    fire.Fire(run)
